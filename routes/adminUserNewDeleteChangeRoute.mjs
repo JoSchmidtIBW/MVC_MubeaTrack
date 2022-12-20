ca38d3f16b7df306642cbbdf0759dac2e750b5b9
John <flnc85@hotmail.de>
2022-12-20 23:57:04 +0100

    Versuch, session

--- routes/adminUserNewDeleteChangeRoute.mjs
+++ routes/adminUserNewDeleteChangeRoute.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import aaaa from '../controllers/adminUserNewDeleteChangeRouteController.mjs';
+import checkAuthentication from "../utils/authentifizierungUser.mjs";
 
 const router = express.Router();
 
@@ -20,6 +21,18 @@ router.get('/',(req,res)=>{
 
 
 // localhost/q/w
-router.get('/w',aaaa)
+//router.get('/w',checkAuthentication,aaaa)
+
+
+
+//router.use("/", authenticateUser); //soll überall verfügbar sein
+// localhost/q  und danach alles
+router.get('/w',checkAuthentication,(req,res)=>{
+    console.log("ich bin adminChange- Route.mjs");
+    console.log("req.body: "+req.body);
+    //header
+    res.send("<h1>Hello adminUserChange-Route  GET</h1>");
+});
+
 
 export default router;
--- utils/authentifizierungUser.mjs
+++ utils/authentifizierungUser.mjs
@@ -1,9 +1,15 @@
 import express from "express";
 
-export default function checkAuthentication(){
+// irgendwas mit express-session        versuch, soll 10sec session haben
+
+
+export default function checkAuthentication(req, res, next){
     let isIrgendwas = false;
     console.log("ich bin checkAuthentication-Funktion in utils/authentifizierungUser.js")
-    return isIrgendwas;
+    //return isIrgendwas;
+    //return next();//next is not defined
+    return res.end("401 Unautorized :) ");
+
 
     /*
         return next();
@@ -17,3 +23,29 @@ export default function checkAuthentication(){
         response.redirect("/login");
      */
 }
+
+
+
+
+//middleware
+/*
+function authenticateUser(req, res, next){
+    res.locals.validatedUsers = "Bob";// sagt ist gut, schreibt in local
+   // res.locals.validatedUsers = "<script>alert('XSS')</script>";//cross side scrippting !!!! zb als name in datenbank
+    console.log("middleware: " + res.locals.validatedUsers)
+    next();
+}
+*/
+
+
+/*
+router.use("/", authenticateUser); //soll überall verfügbar sein
+
+//middleware
+function authenticateUser(req, res, next) {
+    res.locals.validatedUsers = "Bob"; // sagt ist gut, schreibt in local
+    // res.locals.validatedUsers = "<script>alert('XSS')</script>";//cross side scrippting !!!! zb als name in datenbank
+    console.log("middleware: " + res.locals.validatedUsers)
+    next();
+}
+*/
