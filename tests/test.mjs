58b40f95a5fba745bff3f4dfe728e8d4ce6c4741
John <flnc85@hotmail.de>
2023-01-08 18:06:26 +0100

    CI unit-test.... github action testversuch

--- tests/test.mjs
+++ tests/test.mjs
@@ -5,7 +5,7 @@ import sum from '../server.mjs';
 // This is a comment
 describe("Test server.mjs", function() {
     it("Test the sum method", function() {
-        assert.equal(sum(1, 2), 3);
+        assert.equal(sum(1, 2), 4);
     })
 })
 
